991b289a309733db6bb6a2b29214f2c3bc341d6e
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-07 00:14:23 +0900

    Update adding zsh path to /etc/shells

--- etc/init/common/zsh.sh
+++ etc/init/common/zsh.sh
@@ -4,5 +4,5 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME"/.oh-my-zsh
 mkdir -p "$HOME"/.oh-my-zsh/custom/themes
 ln -sfnv "$DOTPATH"/etc/zsh/my_theme.zsh-theme "$HOME"/.oh-my-zsh/custom/themes/my_theme.zsh-theme
 
-echo "/usr/local/bin/zsh" >> /etc/shells
-chsh -s /usr/local/bin/zsh
+echo "/bin/zsh" | sudo tee -a /etc/shells
+chsh -s /bin/zsh
