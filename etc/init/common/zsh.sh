6c880c0c63a31706606e142bf613e3fa31e17e1e
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-07 21:49:40 +0900

    Install zsh before chsh

--- etc/init/common/zsh.sh
+++ etc/init/common/zsh.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ "$PLATFORM" == 'mac' ]; then
+  brew install zsh
+elif [ "$PLATFORM" == 'linux' ]; then
+  sudo apt-get install zsh
+else
+  exit 1
+fi
+
 git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME"/.oh-my-zsh
 mkdir -p "$HOME"/.oh-my-zsh/custom/themes
 ln -sfnv "$DOTPATH"/etc/zsh/my_theme.zsh-theme "$HOME"/.oh-my-zsh/custom/themes/my_theme.zsh-theme
