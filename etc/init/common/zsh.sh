71af60bdcdc739d4252968cf1c18ac62e3c4b45e
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-07 21:40:50 +0900

    Add sudo to `chsh` command

--- etc/init/common/zsh.sh
+++ etc/init/common/zsh.sh
@@ -5,4 +5,4 @@ mkdir -p "$HOME"/.oh-my-zsh/custom/themes
 ln -sfnv "$DOTPATH"/etc/zsh/my_theme.zsh-theme "$HOME"/.oh-my-zsh/custom/themes/my_theme.zsh-theme
 
 echo "/bin/zsh" | sudo tee -a /etc/shells
-chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
